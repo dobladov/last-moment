3b96c720c4fcedaec5e503a3222bc23ef65a13f3
dobladov <danieldoblado@gmail.com>
2016-06-25 17:31:01 +0200

    Initial code upload

---
+++ README.md
@@ -0,0 +1,35 @@
+# Last Moment
+
+Small plugin for [Totem](https://wiki.gnome.org/Apps/Videos) that saves the last time position of your videos 
+
+## Install
+
+Clone the repository on `/home/username/.local/share/totem/plugins`
+
+	cd ~/.local/share/totem/plugins
+    git clone https://github.com/dobladov/last-moment.git
+
+Or download the [files](https://github.com/dobladov/last-moment/archive/master.zip) and copy the folder inside `/home/username/.local/share/totem/plugins`
+
+Enable the plugin on Edit --> Preferences --> Plugins
+
+![Plugins](https://my.mixtape.moe/wrwpsy.png)
+
+## How it works
+
+Any time you open a video a small file tracks the progress with the inode number, so you can change the name, or directory of the file and this plugin still can track the progress.
+
+Just play a previously opened video and it will continue where it was. 
+
+The programr deletes the track of files older than 30 days.
+
+## Documentation
+
++ [Totem Reference Manual](https://developer.gnome.org/totem/stable/)
++ [Writing Plugins for Totem Movie Player](http://asanka-abeyweera.blogspot.com.es/2012/03/writing-plugins-for-totem-movie-player.html)
++ Based on this awesome plugin by [Yauhen-l](https://github.com/yauhen-l) [remember-last-position-totem-plugin
+](https://github.com/yauhen-l/remember-last-position-totem-plugin)
+
+---
+
+![Remember](https://my.mixtape.moe/leoqnh.gif)
---
+++ last-moment.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+
+import json
+import logging
+import threading
+import urllib.parse
+from gi.repository import GObject, Peas
+from os import stat, makedirs, remove
+from dateutil.parser import parse
+from os.path import exists, join
+from datetime import datetime
+from time import time
+
+
+class StarterPlugin (GObject.Object, Peas.Activatable):
+    __gtype_name__ = 'LastMoment'
+
+    object = GObject.property (type = GObject.Object)
+
+    def __init__ (self):
+        GObject.Object.__init__ (self)
+        # logging.basicConfig(level=logging.DEBUG)
+
+    def do_deactivate (self):
+        self._totem = None
+        logging.info("Last-Moment disabled")
+
+    def do_activate (self):
+        self._totem = self.object
+
+        self._totem.connect('file-closed', self.file_closed)
+        self._totem.get_main_window().connect('destroy', self.file_closed)
+        self._totem.connect('file-has-played', self.file_played)
+        self._totem.connect('file-opened', self.file_opened)
+
+        folder = self.plugin_info.get_data_dir() + "/savedTimes"
+
+        if not exists(folder):
+            makedirs(folder)
+        else:
+            self.check_last_clean()
+
+        logging.info("Last-Moment enabled")
+
+    def file_closed(self, to):
+        self.t.cancel()
+        
+    def file_opened(self, to, path):
+        threading.Timer(0.5, self.restore_time).start()
+
+    def file_played(self, to, path):
+        threading.Timer(5, self.set_interval(self.save_time, 3))
+
+    def save_time(self):
+        # Saves a json file with the inode number and time
+        data = {}
+        data['time'] = self.get_time()
+
+        id = self.get_inode()
+        file = self.plugin_info.get_data_dir() + "/savedTimes/" + str(id) + ".json"
+
+        with open(file, 'w') as outfile:
+            json.dump(data, outfile)
+            logging.info("Saved time " + str(data['time']))
+
+    def restore_time(self):
+        ## Finds if a vide have a time file and restores it
+        id = self.get_inode()
+        file = self.plugin_info.get_data_dir() + "/savedTimes/" + str(id) + ".json"
+
+        if (exists(file)):
+            
+            with open(file) as data_file:    
+                data = json.load(data_file)
+                self.set_time(data['time'])
+                logging.info("Restoring to " + str(data['time']))
+                return True
+        else:
+            logging.info("No file found")
+            return False
+
+    def get_time(self):
+        if (self._totem.get_property('seekable')):
+            return self._totem.get_property('current-time')
+        else:
+            return False
+        
+    def set_time(self, time):
+        if (self._totem.get_property('seekable')):
+            logging.info("Go to " + str(time))
+            self._totem.seek_time(time, False)
+        else:
+            return False
+
+    def get_inode(self):
+        url = self._totem.get_property('current-mrl')
+        url = url.replace("file://", "")
+        url = urllib.parse.unquote(url)
+        return stat(url).st_ino
+
+    def set_interval(self, func, sec):
+        def func_wrapper():
+            self.set_interval(func, sec) 
+            func()  
+        
+        self.t = threading.Timer(sec, func_wrapper)
+        self.t.start()
+        return self.t
+
+    def check_last_clean(self):
+        # Checks if is time to delete older files
+        file = self.plugin_info.get_data_dir() + "/lastCheck.json"
+
+        if (exists(file)):
+            
+            with open(file) as data_file:    
+                data = json.load(data_file)
+                lastCheck = data['lastCheck']
+                logging.info("Last check " + lastCheck)
+
+                today = datetime.now()
+                if ((today-parse(lastCheck)).days) > 15:
+                    self.clean_old()
+                    self.set_last_clean()
+
+                return True
+        else:
+            logging.info("Thre is not lastCheck.json file")
+            self.set_last_clean()
+            return False
+        
+    def set_last_clean(self):
+        # Creates a file with the last delete check
+        data = {}
+        data['lastCheck'] = str(datetime.now())
+
+        file = self.plugin_info.get_data_dir() + "/lastCheck.json"
+
+        with open(file, 'w') as outfile:
+            json.dump(data, outfile)
+            logging.info("Saving last check time")
+
+    def clean_old():
+        # Erase older files than 30 days
+        path = self.plugin_info.get_data_dir() + "/savedTimes/"
+
+        for f in os.listdir(path):
+            if (os.stat(join(path,f)).st_mtime < time() - (30 * 86400)):
+                logging.info("Removing " + f)
+                remove(join(path, f))
